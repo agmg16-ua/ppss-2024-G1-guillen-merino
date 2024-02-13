70085e3a7c6d83d28a55d541ae61271fb1cb4c55
agmg16-ua <agmg16@alu.ua.es>
2024-02-13 13:13:10 +0100

    Iniciado ejercicio 2

---
+++ P02-Drivers/drivers/src/test/java/ppss/FicheroTextoTest.java
@@ -0,0 +1,11 @@
+package ppss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FicheroTextoTest {
+
+    @Test
+    void contarCaracteres() {
+    }
+}
