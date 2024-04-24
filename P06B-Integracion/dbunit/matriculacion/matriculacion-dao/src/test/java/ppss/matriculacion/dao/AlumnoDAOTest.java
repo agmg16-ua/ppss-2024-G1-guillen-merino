449498bfcd32c4a0d300dbbefefab4632e39c2b5
agmg16-ua <agmg16@alu.ua.es>
2024-04-24 18:21:30 +0200

    Finalizados tests de integracion y unitarios

---
+++ P06B-Integracion/dbunit/matriculacion/matriculacion-dao/src/test/java/ppss/matriculacion/dao/AlumnoDAOTest.java
@@ -0,0 +1,27 @@
+package ppss.matriculacion.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AlumnoDAOTest {
+
+    @Test
+    public void T1() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void T2() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void T3() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void T4() {
+        Assertions.assertTrue(true);
+    }
+}
