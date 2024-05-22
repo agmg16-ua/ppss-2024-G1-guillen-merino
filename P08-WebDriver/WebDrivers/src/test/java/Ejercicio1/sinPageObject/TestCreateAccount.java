44e5318985136666138efb8163bfb56da63f9023
agmg16-ua <agmg16@alu.ua.es>
2024-05-22 12:34:34 +0200

    Ejercicio 1 terminado

--- P08-WebDriver/WebDrivers/src/test/java/Ejercicio1/sinPageObject/TestCreateAccount.java
+++ P08-WebDriver/WebDrivers/src/test/java/Ejercicio1/sinPageObject/TestCreateAccount.java
@@ -1,13 +1,14 @@
 package Ejercicio1.sinPageObject;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCreateAccount {
 
@@ -18,6 +19,8 @@ public class TestCreateAccount {
         ChromeOptions co = new ChromeOptions();
         co.setBrowserVersion("119");
         driver = new ChromeDriver(co);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("http://demo-store.seleniumacademy.com/");
     }
 
     @AfterEach
@@ -29,9 +32,43 @@ public class TestCreateAccount {
     @Test
     @Tag("OnlyOnce")
     public void S1_scenario_createAccount_should_create_new_account_in_the_demo_store_when_this_account_does_not_exist() {
-        driver.get("http://www.google.com");
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("Packt Publising");
-        searchBox.submit();
+        //Comprobar que el nombre de la pagina es correcto
+        Assertions.assertTrue(driver.getTitle().contains("Madison Island"));
+
+        //Acceder a Log In
+        WebElement account = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a"));
+        account.click();
+        WebElement login = driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a"));
+        login.click();
+
+        //Verificar nombre de pagina Login
+        Assertions.assertTrue(driver.getTitle().contains("Customer Login"));
+
+        //Acceder a Create Account
+        WebElement createAccount = driver.findElement(By.cssSelector("#login-form > div > div.col-1.new-users > div.buttons-set > a"));
+        createAccount.click();
+
+        //Verificar pagina Create Account
+        Assertions.assertTrue(driver.getTitle().contains("Create New Customer Account"));
+
+        //Rellenar datos formulario
+        driver.findElement(By.name("firstname")).sendKeys("Alejandro");
+        driver.findElement(By.name("lastname")).sendKeys("Guillen Merino");
+        driver.findElement(By.name("email")).sendKeys("agmg16@gcloud.ua.es");
+        driver.findElement(By.name("password")).sendKeys("123456");
+        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button")).submit();
+
+        //Verificar mensaje de error
+        Assertions.assertTrue(driver.findElement(By.cssSelector("#advice-required-entry-confirmation")).getText().contains("This is a required field."));
+
+        //Completar informacion
+        driver.findElement(By.name("confirmation")).sendKeys("123456");
+        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button")).submit();
+
+        //Volver a la pagina anterior
+        driver.navigate().back();
+
+        //Comprobar nombre de la pagina
+        Assertions.assertTrue(driver.getTitle().contains("My Account"));
     }
 }
--- P08-WebDriver/WebDrivers/src/test/java/Ejercicio1/sinPageObject/TestLogin.java
+++ P08-WebDriver/WebDrivers/src/test/java/Ejercicio1/sinPageObject/TestLogin.java
@@ -1,12 +1,87 @@
 package Ejercicio1.sinPageObject;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
 public class TestLogin {
 
-    public void S2_scenario_loginOK_should_login_with_success_when_user_account_exists() {
+    WebDriver driver;
 
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions co = new ChromeOptions();
+        co.setBrowserVersion("119");
+        driver = new ChromeDriver(co);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("http://demo-store.seleniumacademy.com/");
     }
 
+    @AfterEach
+    public void toClose() {
+        driver.quit();
+    }
+
+    @Test
+    public void S2_scenario_loginOK_should_login_with_success_when_user_account_exists() {
+        //Comprobar que la pagina ha cargado
+        Assertions.assertTrue(driver.getTitle().contains("Madison Island"));
+
+        //Acceder a la pagina de Log In
+        WebElement account = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a"));
+        account.click();
+        WebElement logIn = driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a"));
+        logIn.click();
+
+        //Verificar nombre de pagina Login
+        Assertions.assertTrue(driver.getTitle().contains("Customer Login"));
+
+        //Rellenar solo email y submit
+        driver.findElement(By.name("login[username]")).sendKeys("agmg16@gcloud.ua.es");
+        driver.findElement(By.cssSelector("#send2")).submit();
+
+        //Comprobar mensaje de error
+        Assertions.assertTrue(driver.findElement(By.cssSelector("#advice-required-entry-pass")).getText().contains("This is a required field."));
+
+        //Introducir contraseña y reenviar
+        driver.findElement(By.name("login[password]")).sendKeys("123456");
+        driver.findElement(By.cssSelector("#send2")).submit();
+
+        //Comprobar que la pagina es correcta
+        Assertions.assertTrue(driver.getTitle().contains("My Account"));
+    }
+
+    @Test
     public void S3_scenario_loginFailed_should_fail_when_user_account_not_exists() {
+        //Comprobar que la pagina ha cargado
+        Assertions.assertTrue(driver.getTitle().contains("Madison Island"));
+
+        //Acceder a la pagina de Log In
+        WebElement account = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a"));
+        account.click();
+        WebElement logIn = driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a"));
+        logIn.click();
+
+        //Verificar nombre de pagina Login
+        Assertions.assertTrue(driver.getTitle().contains("Customer Login"));
+
+        //Introducir contraseña erronea
+        driver.findElement(By.name("login[username]")).sendKeys("agmg16@gcloud.ua.es");
+        driver.findElement(By.name("login[password]")).sendKeys("123456789");
+        driver.findElement(By.cssSelector("#send2")).submit();
+
+        //Verificar mensaje de error
+        Assertions.assertTrue(driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col1-layout > div > div > div.account-login > ul > li > ul > li > span")).getText().contains("Invalid login or password."));
+
+
 
     }
 
